94a1853abb341e525fc88c33e54801ccfa09a387
Saomonite <981003163@qq.com>
2017-10-30 23:24:15 +0800

    在线简历编辑

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="page">
+    <header>
+      <Topbar/>
+    </header>
+    <main>
+    <ResumeEditor/>
+    <ResumePreview/>
+    </main>
+    
+  </div>
+</template>
+
+<script>
+import './assets/reset.css'
+import 'normalize.css/normalize.css' 
+
+import Topbar from './components/Topbar'
+import ResumeEditor from './components/ResumeEditor'
+import ResumePreview from './components/ResumePreview'
+import icons from './assets/icons'
+
+export default {
+  name: 'app',
+   data: function(){
+     return {		    
+     }		     
+   },		   
+  components: {
+    Topbar, ResumeEditor, ResumePreview
+  },
+  created() {
+    document.body.insertAdjacentHTML('afterbegin',icons)
+  }
+}
+</script>
+
+<style>
+#page{
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: #eaebec;
+}
+#page>main{
+  flex: 1;
+  min-width: 1024px;
+  max-width: 1440px;
+  margin-top: 16px;
+  margin-bottom: 16px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 16px;
+  width: 100%;
+  align-self: center;
+}
+#ResumeEditor{
+  width:35%;  
+  background: #444;
+}
+#ResumePreview{
+  flex: 1;
+  margin-left: 16px;
+  background: #777;
+}
+svg.icon{
+  height: 1em;
+  width: 1em;
+  fill: currentColor;
+  vertical-align: -0.1em;
+  font-size: 16px;
+}
+</style>
---
+++ src/components/ResumeEditor.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="resumeEditor">
+      <nav>
+          <ol>
+              <li v-for="(item,index) in resume.config"
+              :class="{active: item.field === selected}" 
+              @click="selected = item.field">
+              {{index}}
+              <svg class="icon">
+                  <use :xlink:href="`#icon-${item.icon}`"></use>
+              </svg>
+              </li>
+          </ol>
+      </nav>
+      <ol class="panels">
+          <li v-for="item in resume.config"
+          v-show="item.field === selected">
+          {{resume[item.field]}}
+          </li> 
+      </ol>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ResumeEditor',
+    data() {
+        return {
+            selected: 'profile',
+            resume: {
+                config: [
+            { field: 'profile', icon: 'id' },
+            { field: 'work history', icon: 'work' },
+            { field: 'education', icon: 'book' },
+            { field: 'projects', icon: 'heart' },
+            { field: 'awards', icon: 'cup' },
+            { field: 'contacts', icon: 'phone' },
+                ],
+                profile: {
+                    name: '',
+                    city: '',
+                    title: '',
+                },
+                'work history': [],
+                education: [],
+                projects: [],
+                awards: [''],
+                contacts: [],
+                others: []
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+#resumeEditor{
+    background: #fff;
+    box-shadow: 0 1px 3px 0 rgba(0,0,0,0.25);
+    display: flex;
+    flex-direction: row;
+    overflow: auto;
+    >nav{
+        width: 80px;
+        background: #000;
+        color: #fff;
+        >ol{
+            >li{
+                height: 48px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                margin-top: 16px;
+                margin-bottom: 16px;
+                &.active{
+                    background: #fff;
+                    color: #000;
+                }
+            }
+        }
+    }
+    svg.icon{
+        width: 24px;
+        height: 24px;
+    }
+    
+}
+ol{
+    list-style: none;
+}
+</style>
